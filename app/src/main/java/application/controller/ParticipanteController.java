f5d761877fcb254f909b9f2f7c28829c30e825c7
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2024-04-30 19:25:16 +0000

    Criado o a requisição GET ONE e seu teste em http

--- app/src/main/java/application/controller/ParticipanteController.java
+++ app/src/main/java/application/controller/ParticipanteController.java
@@ -1,9 +1,14 @@
 package application.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import application.model.Participante;
 import application.repository.ParticipanteRepository;
@@ -17,5 +22,16 @@ public class ParticipanteController {
     @GetMapping
     public Iterable<Participante> getAll() {
         return participanteRepo.findAll();
+    }
+
+    @GetMapping("/id")
+    public Participante getOne(@PathVariable long id) {
+        Optional<Participante> result = participanteRepo.findById(id);
+        if(result.isEmpty()) {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Participante não encontrado"
+            );
+        }
+        return result.get();
     }
 }
