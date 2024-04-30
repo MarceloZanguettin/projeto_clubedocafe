fbb56a0c3114aa60431026acb06ef48d35f917f1
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2024-04-30 19:06:00 +0000

    Criado o repositorio da entidade Compra Possui Produto

---
+++ app/src/main/java/application/repository/CompraPossuiProdutoRepository.java
@@ -0,0 +1,8 @@
+package application.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import application.model.CompraPossuiProduto;
+
+public interface CompraPossuiProdutoRepository extends CrudRepository<CompraPossuiProduto, Long> {    
+}
