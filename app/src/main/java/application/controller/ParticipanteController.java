81ee6c2e018eb3ba79e19dfb3bc50f98fcb4f267
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2024-04-30 19:29:06 +0000

    arrumado o bug

--- app/src/main/java/application/controller/ParticipanteController.java
+++ app/src/main/java/application/controller/ParticipanteController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -24,7 +25,7 @@ public class ParticipanteController {
         return participanteRepo.findAll();
     }
 
-    @GetMapping("/id")
+    @GetMapping("/{id}")
     public Participante getOne(@PathVariable long id) {
         Optional<Participante> result = participanteRepo.findById(id);
         if(result.isEmpty()) {
