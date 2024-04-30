574c49ec351790d8131e1b4842ce7473e483bece
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2024-04-30 19:34:17 +0000

    Criado a requisição POST e seu teste em http

--- app/src/main/java/application/controller/ParticipanteController.java
+++ app/src/main/java/application/controller/ParticipanteController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -34,5 +35,10 @@ public class ParticipanteController {
             );
         }
         return result.get();
+    }
+
+    @PostMapping
+    private Participante post(@RequestBody Participante participante) {
+        return participanteRepo.save(participante);
     }
 }
