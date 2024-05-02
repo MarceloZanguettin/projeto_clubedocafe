edf5774bdcec0aa5924c23c123dabdb31a7e2a09
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2024-05-02 20:04:12 +0000

    Criado o controlador do comprovante com as requições necessárias

---
+++ app/src/main/java/application/controller/ComprovanteController.java
@@ -0,0 +1,74 @@
+package application.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import application.model.Comprovante;
+import application.repository.ComprovanteRepository;
+
+@RestController
+@RequestMapping("/comprovantes")
+public class ComprovanteController {
+    @Autowired
+    private ComprovanteRepository comprovanteRepo;
+
+    @GetMapping
+    public Iterable<Comprovante> getAll() {
+        return comprovanteRepo.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Comprovante getOne(@PathVariable long id) {
+        Optional<Comprovante> result = comprovanteRepo.findById(id);
+        if(result.isEmpty()) {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Comprovante não encontrado"
+            );
+        }
+
+        return result.get();
+    }
+
+    @PostMapping
+    public Comprovante post(@RequestBody Comprovante comprovante) {
+        return comprovanteRepo.save(comprovante)
+    }
+
+    @PutMapping("/{id}")
+    private Comprovante put(@RequestBody Comprovante comprovante, @PathVariable long id) {
+        Optional<Comprovante> result = comprovanteRepo.findById(id);
+        if(result.isEmpty()) {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Comprovante não encontrado"
+            );
+        }
+
+        result.get().setParticipante(comprovante.getParticipante());
+        result.get().setValor(comprovante.getValor());
+        result.get().setMes(comprovante.getMes());
+        result.get().setDataPagamento(comprovante.getDataPagamento());
+        return comprovanteRepo.save(result.get());
+    }
+
+    @DeleteMapping("/{id}")
+    private void delete(@PathVariable long id) {
+        if(comprovanteRepo.existsById(id)) {
+            comprovanteRepo.deleteById(id);
+        } else {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Comprovante não encontrado"
+            );
+        }
+    }
+}
