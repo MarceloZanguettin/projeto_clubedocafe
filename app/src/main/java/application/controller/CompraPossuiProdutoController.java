a62a1441a7efafa69d76ce4ffe292ec7d6287b7c
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2024-06-09 22:08:21 +0000

    .

--- app/src/main/java/application/controller/CompraPossuiProdutoController.java
+++ app/src/main/java/application/controller/CompraPossuiProdutoController.java
@@ -3,10 +3,20 @@ package application.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import application.model.CompraPossuiProduto;
+import application.model.ListaCompra;
+import application.model.Produto;
 
 @RestController
 @RequestMapping("/comprapossuiprodutos")
@@ -15,5 +25,75 @@ public class CompraPossuiProdutoController {
     private CompraPossuiProduto compraPossuiProdutoRepo;
 
     @Autowired
-    private 
+    private ListaCompra listaCompraRepo;
+
+    @Autowired
+    private Produto produtoRepo;
+
+    @GetMapping
+    public Iterable<CompraPossuiProduto> getAll() {
+        return compraPossuiProdutoRepo.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public CompraPossuiProduto getOne(@PathVariable long id) {
+        Optional<CompraPossuiProduto> result = compraPossuiProdutoRepo.findById(id);
+        if(result.isEmpty()) {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Compra possui Produto não Encontrada"
+            );
+        }
+        return result.get();
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id) {
+        if(compraPossuiProdutoRepo.existById(id)) {
+            compraPossuiProdutoRepo.deleteById(id);
+        } else {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Compra possui Produto não Encontrada"
+            );
+        }
+    }
+
+    @PostMapping
+    public CompraPossuiProduto post(@RequestBody CompraPossuiProduto compraPossuiProduto) {
+        if(!listaCompraRepo.existById(CompraPossuiProduto.getListaCompra())) {
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST, "Lista de Compra vinculada não Encontrada"
+            );
+        } else if(!produtoRepo.existById(CompraPossuiProduto.getProduto())){
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST, "Poduto vinculado não Encontrado"
+            );
+        }
+        return compraPossuiProdutoRepo.save(compraPossuiProduto);
+    }
+
+    @PutMapping("/{id}")
+    public CompraPossuiProduto put(@RequestBody CompraPossuiProduto compraPossuiProduto, @PathVariable long id) {
+        Optional<CompraPossuiProduto> result = compraPossuiProdutoRepo.findById(id);
+        if(result.isEmpty()) {
+            throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, "Compra possui Produto não Encontrada"
+            );
+        }
+        if(!listaCompraRepo.existById(CompraPossuiProduto.getListaCompra())) {
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST, "Lista de Compra vinculada não Encontrada"
+            );
+        } else if(!produtoRepo.existById(CompraPossuiProduto.getProduto())){
+            throw new ResponseStatusException(
+                HttpStatus.BAD_REQUEST, "Poduto vinculado não Encontrado"
+            );
+        }
+
+        result.get().setListaCompra(compraPossuiProduto.getListaCompra());
+        result.get().setProduto(compraPossuiProduto.getProduto());
+        result.get().setQuantidade(compraPossuiProduto.getQuantidade());
+        result.get().setValor(compraPossuiProduto.getValor());
+
+        return compraPossuiProdutoRepo.save(result.get());
+    }
 }
